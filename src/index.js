611f84ca83b86e7c396035c1f91dff526a1fb591
yoojm4718 <yoojm4718@outlook.com>
2021-08-27 22:45:42 +0900

    Created WS Server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import WebSocket from "ws";
+import http from "http";
 
 const app = express();
 const PORT = 5000;
@@ -8,8 +10,13 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () =>
   console.log(`Listening on http://localhost:${PORT}/`);
 
-app.listen(PORT, handleListen);
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+server.listen(PORT, handleListen);
